787368e4350e59a8e5b9912b77d9cfa5a8f6bc87
Eran Rivlis <eran.rivlis@yahoo.com>
2025-08-31 02:03:58 +0300

    refactor: simplify graph type determination and enhance documentation

--- src/networkx_graphql.py
+++ src/networkx_graphql.py
@@ -5,18 +5,17 @@ from graphinate.builders import GraphQLBuilder
 from graphinate.typing import Extractor
 
 
-def graph_type(graph: nx.Graph) -> GraphType:
-    if graph.is_directed() and graph.is_multigraph():
-        return GraphType.MultiDiGraph
-    elif graph.is_directed():
-        return GraphType.MultiGraph
-    elif graph.is_multigraph():
-        return GraphType.MultiDiGraph.MultiGraph
-    else:
-        return GraphType.Graph
-
-
 def schema(graph: nx.Graph, node_type_extractor: Extractor | None = None) -> strawberry.Schema:
+    """Generate a Strawberry GraphQL schema from a NetworkX graph.
+
+    Args:
+        graph (nx.Graph): The input NetworkX graph.
+        node_type_extractor (Extractor | None, optional): A function to extract node types from the graph nodes.
+            If None, all nodes will be treated as the same type 'node'. Defaults to None.
+
+    Returns:
+        strawberry.Schema: The generated GraphQL schema.
+    """
     graph_model = GraphModel(name=graph.name)
 
     node_type_extractor = node_type_extractor if callable(node_type_extractor) else 'node'
@@ -30,10 +29,16 @@ def schema(graph: nx.Graph, node_type_extractor: Extractor | None = None) -> str
         for edge in graph.edges:
             yield {'source': edge[0], 'target': edge[1]}
 
-    graphql_builder = GraphQLBuilder(graph_model, graph_type=graph_type(graph))
+    graphql_builder = GraphQLBuilder(graph_model, graph_type=GraphType.of(graph))
 
     return graphql_builder.build()
 
 
-def run(schema: strawberry.Schema, port: 8073) -> dict:
-    return graphql(schema, port)
+def server(graphql_schema: strawberry.Schema, port: int = 8073):
+    """Run a GraphQL server with the given schema on the specified port.
+
+    Args:
+        graphql_schema (strawberry.Schema): The Strawberry GraphQL schema to serve.
+        port (int, optional): The port number to run the server on. Defaults to 8073.
+    """
+    graphql.server(graphql_schema=graphql_schema, port=port)
